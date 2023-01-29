6296b3b9dba5220c84f5e30a311b206ade2bc06e
ZwingliCaleb <calebzwingli@gmail.com>
2023-01-29 11:42:44 -0800

    factor.h added

--- factor.h
+++ factor.h
@@ -1,32 +1,13 @@
-#include "factor.h"
-
-/**
- * main - main function
- *
- * Return: void
- */
-int main(int argc, char *argv[])
-{
-	FILE *fptr;
-	size_t count;
-	ssize_t line;
-	char *buffer = NULL;
+#ifndef _FACTOR_H_
+#define _FACTOR_H_
 
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: factor <filename>\n");
-		exit(EXIT_FAILURE);
-	}
-	fptr = fopen(argv[1], "r");
-	if (fptr == NULL)
-	{
-		fprintf(stderr, "Error: can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while((line getine(&buffer, &count, fptr)) != -1)
-	{
-		factorize(buffer);
-	}
-return (0);
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define _GNU_SOURCE
+
+/* size_t getline(char **lineptr, size_t *n, FILE *stream); */
+int factorize(char *buffer);
+
+#endif /* _FACTOR_H_ */
